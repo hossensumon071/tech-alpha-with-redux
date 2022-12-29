399f291f93f51c6b8708d0134496c5a1b188e9d3
hossensumon071 <55638643+hossensumon071@users.noreply.github.com>
2022-12-29 19:57:12 +0600

    slider component added

---
+++ src/components/Slider.jsx
@@ -0,0 +1,55 @@
+const data = [
+  {
+    id: 1,
+    src: "https://images.pexels.com/photos/1787236/pexels-photo-1787236.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    headline: "DSLR cameras for stunning photos",
+    body: "Are you an aspiring photographer looking to take your skills to the next level? Our DSLR cameras offer advanced features and high-quality image sensors to help you capture stunning photos. From landscape shots to portraits, these cameras are perfect for capturing all types of subjects.",
+    cta: "Shop DSLR cameras now",
+  },
+  {
+    id: 2,
+    src: "https://images.pexels.com/photos/1444416/pexels-photo-1444416.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    headline: "Upgrade your home entertainment with our TVs",
+    body: "Experience the latest in home entertainment with our selection of TVs. From sleek and modern designs to advanced features like 4K resolution and smart capabilities, our TVs will bring your favorite movies, TV shows, and streaming content to life.",
+    cta: "Shop TVs and upgrade now",
+  },
+  {
+    id: 3,
+    src: "https://images.pexels.com/photos/1649771/pexels-photo-1649771.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    headline: "Enhance your listening experience",
+    body: "Take your music, movies, and more to the next level with our headphones. Our selection offers a range of styles and features, including noise-cancelling technology, wireless connectivity, and comfortable designs for all-day wear.",
+    cta: "Experience enhanced sound",
+  },
+  {
+    id: 4,
+    src: "https://images.pexels.com/photos/1174746/pexels-photo-1174746.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    headline: "Take your gaming to the next level",
+    body: "Elevate your gaming experience with our selection of gaming consoles. From the latest models to classic systems, we have a console for every type of gamer. Our consoles offer advanced graphics, fast processing speeds, and a variety of exclusive games to choose from.",
+    cta: "Shop consoles and play now",
+  },
+  {
+    id: 5,
+    src: "https://images.pexels.com/photos/393047/pexels-photo-393047.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    headline: "Stay connected with smart watches",
+    body: "Stay connected and on top of your day with our smart watches. Our selection offers a range of styles and features, including fitness tracking, phone notifications, and voice assistants. These watches are the perfect combination of functionality and style.",
+    cta: "Connect with a smart watch",
+  },
+];
+
+const Slider = () => {
+  return (
+    <div className="slider">
+      {
+        data.map((image) => (
+          <div
+          className="slide"
+          style={{ backgroundImage:  `url(${image.src})`}}
+          key={image.id}
+          ></div>
+        ))
+      }
+    </div>
+  );
+};
+
+export default Slider;
