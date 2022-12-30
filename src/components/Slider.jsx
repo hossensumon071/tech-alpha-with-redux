e54515d3ced5255263f29191670c6eadd33388ac
hossensumon071 <55638643+hossensumon071@users.noreply.github.com>
2022-12-30 08:18:36 +0600

    slider added

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,3 +1,6 @@
+import { useState } from 'react';
+import { BsArrowLeft, BsArrowRight } from 'react-icons/bs';
+
 const data = [
   {
     id: 1,
@@ -37,17 +40,51 @@ const data = [
 ];
 
 const Slider = () => {
+  const [currentSlide, setCurrentSlide] = useState(0) // for slide
+  // slider handler function
+  const prevSlide = () => {
+    setCurrentSlide(
+      currentSlide === 0 ? data.length - 1 : (prevSlide) => prevSlide - 1
+    );
+  };
+
+  const nextSlide = () => {
+    setCurrentSlide(
+      currentSlide === data.length - 1 ? 0 : (prevSlide) => prevSlide + 1
+    );
+  };
   return (
-    <div className="slider">
-      {
-        data.map((image) => (
+    <div className="frame relative">
+       <div
+        className="slider"
+        style={{ transform: `translateX(-${100 * currentSlide}vw)` }}
+      >
+        {data.map((image) => (
           <div
-          className="slide"
-          style={{ backgroundImage:  `url(${image.src})`}}
-          key={image.id}
+            className="slide"
+            style={{ backgroundImage: `url(${image.src})` }}
+            key={image.id}
           ></div>
-        ))
-      }
+        ))}
+      </div>
+      <div className="btns absolute left-0 right-0 bottom-20 m-auto w-fit z-[1] text-white text-2xl flex gap-10">
+        <button
+          onClick={prevSlide}
+          className="prev-btn h-14 w-20 bg-white/40 backdrop-blur-xl flex justify-center items-center hover:bg-white/90 hover:text-black duration-300"
+        >
+          <span>
+            <BsArrowLeft />
+          </span>
+        </button>
+        <button
+          onClick={nextSlide}
+          className="next-btn h-14 w-20 bg-white/40 backdrop-blur-xl flex justify-center items-center hover:bg-white/90 hover:text-black duration-300"
+        >
+          <span>
+            <BsArrowRight />
+          </span>
+        </button>
+      </div>
     </div>
   );
 };
