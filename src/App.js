6aee797ccc29ed79843e8913f6afd938cbacb46c
hossensumon071 <55638643+hossensumon071@users.noreply.github.com>
2023-01-10 11:02:54 +0600

    porducts parts done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import { Route, Routes } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import Home from './pages/Home';
-import Products from './pages/Products';
-import Cart from './pages/Cart';
-import NotFound from './pages/NotFound';
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+import Cart from "./pages/Cart";
+import NotFound from "./pages/NotFound";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-function App() {
+const App = () => {
   return (
     <>
-    <div className="App min-h-screen bg-gray-50 text-gray-700">
-      <Navbar/>
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/products" element={<Products/>} />
-        <Route path="/cart" element={<Cart/>}/>
-        <Route path="*" element={<NotFound/>}/>
-      </Routes>
-     </div>
-     <Footer/>
+      <div className="app min-h-screen bg-gray-50 text-gray-700">
+        <Navbar />
+        <ToastContainer />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/products/:id" element={<Products />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
+      <Footer />
     </>
   );
-}
+};
 
 export default App;
